9e94dbea5b9a9daf3265b23f38d2e3aa57c4793c
itex-04 <32319840+IT-tomoya@users.noreply.github.com>
2019-07-25 02:34:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
+import urllib2
+from bs4 import BeautifulSoup
+
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,10 +19,25 @@ async def ping(ctx):
     
 @bot.command()
 async def pubg(ctx, arg):
-    await ctx.send(arg)
+    await ctx.send(get_status(arg))
     
-@bot.listen()
-async def on_message(message):
-    await ctx.send('listen')
+
+def get_status(arg):
+    ret_text = ""
+    # アクセスするURL
+    url = "https://dak.gg/profile/" + arg + "/pc-2018-04/steam"
     
+    # URLにアクセスする 戻り値にはアクセスした結果やHTMLなどが入ったinstanceが帰ってきます
+    instance = urllib2.urlopen(url)
+
+    # instanceからHTMLを取り出して、BeautifulSoupで扱えるようにパースします
+    soup = BeautifulSoup(instance, "html.parser")
+
+    # CSSセレクターを使って指定した場所のtextを表示します
+    ret_text += "KD:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.kd.stats-item.stats-top-graph > p").text
+    ret_text += "平均ダメージ:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.deals.stats-item.stats-top-graph > p").text 
+    ret_text += "ゲーム数:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.games.stats-item.stats-top-graph > p").text
+    ret_text += "最高キル:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.mostkills.stats-item.stats-top-graph > p").text
+    return ret_text
+
 bot.run(token)

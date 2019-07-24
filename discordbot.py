61bb5723854ee0dc67241531fd4af76d964f374d
itex-04 <32319840+IT-tomoya@users.noreply.github.com>
2019-07-25 01:58:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,23 +14,11 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
+    await ctx.send('pong')
+    
+@bot.command()
+async def pubg(ctx):
     await ctx.send(ctx.content)
     
-    
-@client.event
-async def on_ready():
-    print('wake up')
-    
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-   
-
-
 client.run("token")
 #bot.run(token)

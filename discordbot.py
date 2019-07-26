a4acee1fc185a5da56e26b985dca77460510f101
itex-04 <32319840+IT-tomoya@users.noreply.github.com>
2019-07-27 03:20:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+driver_path = os.environ['GOOGLE_CHROME_SHIM']
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -24,33 +24,36 @@ async def pubg(ctx, arg):
     url = "https://pubg.op.gg/user/" + arg
     
     # URLにアクセスする 戻り値にはアクセスした結果やHTMLなどが入ったinstanceが帰ってきます
-    instance = requests.get(url)  
+#     instance = requests.get(url)  
 
-    # instanceからHTMLを取り出して、BeautifulSoupで扱えるようにパースします
-    soup = BeautifulSoup(instance.text, "html.parser")
+#     # instanceからHTMLを取り出して、BeautifulSoupで扱えるようにパースします
+#     soup = BeautifulSoup(instance.text, "html.parser")
 
-    # CSSセレクターを使って指定した場所のtextを表示します
-    #ret_text1 = "KD:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.kd.stats-item.stats-top-graph > p").text
-    ret_text1 = "KD:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(1) > div:nth-child(2)").text
-    ret_text1 = ret_text1.replace("\n", "")
-    #ret_text2 = "平均ダメージ:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.deals.stats-item.stats-top-graph > p").text 
+#     # CSSセレクターを使って指定した場所のtextを表示します
+#     #ret_text1 = "KD:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.kd.stats-item.stats-top-graph > p").text
+#     ret_text1 = "KD:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(1) > div:nth-child(2)").text
+#     ret_text1 = ret_text1.replace("\n", "")
+#     #ret_text2 = "平均ダメージ:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.deals.stats-item.stats-top-graph > p").text 
     
-    ret_text2 = "平均ダメージ:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(2) > div:nth-child(2)").text
-    ret_text2 = ret_text2.replace("\n", "")
+#     ret_text2 = "平均ダメージ:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(2) > div:nth-child(2)").text
+#     ret_text2 = ret_text2.replace("\n", "")
     
-    #ret_text3 = "ゲーム数:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.games.stats-item.stats-top-graph > p").text
-    ret_text3 = "ゲーム数:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(1)").text
-    ret_text3 = ret_text3.replace("\n", "")
+#     #ret_text3 = "ゲーム数:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.games.stats-item.stats-top-graph > p").text
+#     ret_text3 = "ゲーム数:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(1)").text
+#     ret_text3 = ret_text3.replace("\n", "")
     
-    #ret_text4 = "最高キル:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.mostkills.stats-item.stats-top-graph > p").text
-    ret_text4 = "最高キル:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(10) > div.ranked-stats__value").text
-    ret_text4 = ret_text4.replace("\n", "")
+#     #ret_text4 = "最高キル:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.mostkills.stats-item.stats-top-graph > p").text
+#     ret_text4 = "最高キル:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(10) > div.ranked-stats__value").text
+#     ret_text4 = ret_text4.replace("\n", "")
+    driver = webdriver.Chrome(driver_path)
+    driver.get(URL)
+    ret_text1 = driver.find_element_by_class_name("ranked-stats__value ranked-stats__value--imp ranked-stats__value--good").text
+    driver.close()
+    driver.quit()
+    
     
     ret_text = ret_text1 + "\n" + ret_text2 + "\n" + ret_text3 + "\n" + ret_text4 
     await ctx.send(ret_text.replace(" ", ""))
-    await ctx.send(ret_text1.replace(" ", ""))
-    await ctx.send(ret_text2.replace(" ", ""))
-    await ctx.send(ret_text3.replace(" ", ""))
-    await ctx.send(ret_text4.replace(" ", ""))
+
 
 bot.run(token)

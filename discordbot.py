6d82adf3a2f9e8be3974dd911abb5664d7ba0c1f
itex-04 <32319840+IT-tomoya@users.noreply.github.com>
2019-07-30 03:12:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,33 +22,33 @@ async def ping(ctx):
     
 @bot.command()
 async def pubg(ctx, arg):
-    ret_text = ""
-    # アクセスするURL
-    url = "https://dak.gg/profile/" + arg + "/pc-2018-04/steam"
+#     ret_text = ""
+#     # アクセスするURL
+#     url = "https://dak.gg/profile/" + arg + "/pc-2018-04/steam"
     
-    # URLにアクセスする 戻り値にはアクセスした結果やHTMLなどが入ったinstanceが帰ってきます
-    instance = requests.get(url)  
+#     # URLにアクセスする 戻り値にはアクセスした結果やHTMLなどが入ったinstanceが帰ってきます
+#     instance = requests.get(url)  
 
-    # instanceからHTMLを取り出して、BeautifulSoupで扱えるようにパースします
-    soup = BeautifulSoup(instance.text, "html.parser")
+#     # instanceからHTMLを取り出して、BeautifulSoupで扱えるようにパースします
+#     soup = BeautifulSoup(instance.text, "html.parser")
 
-    # CSSセレクターを使って指定した場所のtextを表示します
-    ret_text1 = "KD:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.kd.stats-item.stats-top-graph > p").text
-    ret_text1 = ret_text1.replace("\n", "")
-    ret_text2 = "平均ダメージ：" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.deals.stats-item.stats-top-graph > p").text
-    ret_text2 = ret_text2.replace("\n", "")
-    ret_text3 = "ゲーム数:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.games.stats-item.stats-top-graph > p").text
-    ret_text3 = ret_text3.replace("\n", "")
-    ret_text4 = "最高キル:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.mostkills.stats-item.stats-top-graph > p").text
-    ret_text4 = ret_text4.replace("\n", "")
+#     # CSSセレクターを使って指定した場所のtextを表示します
+#     ret_text1 = "KD:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.kd.stats-item.stats-top-graph > p").text
+#     ret_text1 = ret_text1.replace("\n", "")
+#     ret_text2 = "平均ダメージ：" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.deals.stats-item.stats-top-graph > p").text
+#     ret_text2 = ret_text2.replace("\n", "")
+#     ret_text3 = "ゲーム数:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.games.stats-item.stats-top-graph > p").text
+#     ret_text3 = ret_text3.replace("\n", "")
+#     ret_text4 = "最高キル:" + soup.select_one("#profile > div.profileContent.season-19.steam > div.modeSummary > section.squad.modeItem > div.mode-section.fpp > div.stats > div.mostkills.stats-item.stats-top-graph > p").text
+#     ret_text4 = ret_text4.replace("\n", "")
 
-    #ret_text1 = "KD:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(1) > div:nth-child(2)").text
-    #ret_text2 = "平均ダメージ：" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(2) > div:nth-child(2)").text
-    #ret_text3 = "ゲーム数：" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(1)").text
-    #ret_text4 = "最高キル：" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(10) > div.ranked-stats__value").text
+#     #ret_text1 = "KD:" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(1) > div:nth-child(2)").text
+#     #ret_text2 = "平均ダメージ：" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(2) > div:nth-child(2)").text
+#     #ret_text3 = "ゲーム数：" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > div > div:nth-child(2) > div:nth-child(1)").text
+#     #ret_text4 = "最高キル：" + soup.select_one("#rankedStatsWrap > div.ranked-stats-wrapper__list > div:nth-child(95) > div > div:nth-child(3) > div > div > div > ul > li:nth-child(10) > div.ranked-stats__value").text
     
-    ret_text = "プレイヤーネーム：" + arg + "\n" + ret_text1 + ret_text2 + "\n" + ret_text3 + ret_text4 
-    await ctx.send(ret_text.replace(" ", ""))
+#     ret_text = "プレイヤーネーム：" + arg + "\n" + ret_text1 + ret_text2 + "\n" + ret_text3 + ret_text4 
+#     await ctx.send(ret_text.replace(" ", ""))
 
     
     url2 = "https://pubg.op.gg/user/" + arg
@@ -65,7 +65,7 @@ async def pubg(ctx, arg):
     # ブラウザでアクセスする
     driver.get(url2)
     
-    await ctx.send("さつえいちゅうだよ！！")
+    #await ctx.send("さつえいちゅうだよ！！")
     time.sleep(2)
     
     page_width = driver.execute_script('return document.body.scrollWidth')
@@ -76,7 +76,7 @@ async def pubg(ctx, arg):
         im = im.crop((0, 385, 998, 960))  # (9)
         im.save("screenshot_crop.png")  # (10)
         im.close()
-        await ctx.send(arg, file=discord.File('screenshot_crop.png', 'ss.png'))
+        await ctx.send("プレイヤーネーム："arg, file=discord.File('screenshot_crop.png', 'ss.png'))
     else: 
         await ctx.send('さつえいしっぱい')
     driver.quit()
